5fbe87595802ba7865bd016ad6b2b232e5acec6f
hkhy2145 <132103457+hkhy2145@users.noreply.github.com>
2023-05-08 18:51:59 +0100

    GCngrok.sh

---
+++ GCngrok.sh
@@ -0,0 +1,8 @@
+rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
+echo "======================="
+echo "Download ngrok"
+echo "======================="
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok.zip > /dev/null 2>&1
+read -p "Paste Ngrok Authtoken: " CRP
+./ngrok 2P4NtDmF470ZwPvy8b7OBWt9bZO_4pXR6ghoH4g5meoPDJnts $CRP 
